721c07628f2018b1ad79555f7537fd9deea6274b
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-08 03:29:13 +0300

    SQL_5

--- console.sql
+++ console.sql
@@ -3,3 +3,11 @@ CREATE DATABASE company_repository;
 CREATE SCHEMA company_storage;
 
 DROP SCHEMA company_storage;
+
+CREATE TABLE company_storage.company (
+    id INT,
+    name VARCHAR(128),
+    date DATE
+);
+
+-- DROP TABLE public.company;
