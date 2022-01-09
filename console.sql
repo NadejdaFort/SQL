a5028317312b932c12dc49ac4860f975026f86c4
Nadejda Doborina <NadejdaFort@gmail.com>
2022-01-10 01:25:09 +0300

    SQL_8

--- console.sql
+++ console.sql
@@ -40,6 +40,17 @@ CREATE TABLE employee
 DROP TABLE employee;
 
 INSERT INTO employee (first_name, last_name, salary)
-VALUES ('Ivan', 'Ivanov', 1000),
+VALUES ('Ivan', 'Sidorov', 500),
+       ('Ivan', 'Ivanov', 1000),
        ('Petr', 'Petrov', 2000),
        ('Sveta', 'Svetikova', 1500);
+
+SELECT DISTINCT
+       id,
+       first_name f_name,
+       last_name l_name,
+       salary
+FROM employee empl
+ORDER BY first_name, salary DESC
+LIMIT 2
+OFFSET 2;
